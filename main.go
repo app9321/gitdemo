84edba7fae73e1a44dc0638563f8eeb035bb542d
pubblic <pubblic256@gmail.com>
2018-11-08 04:06:36 +0000

    1.5초 이후 이벤트 발생

--- main.go
+++ main.go
@@ -4,24 +4,31 @@ import "time"
 import "fmt"
 
 func main() {
-    c1 := make(chan string)
-    c2 := make(chan string)
+	c1 := make(chan string)
+	c2 := make(chan string)
+	c3 := make(chan string)
 
-    go func() {
-        time.Sleep(1 * time.Second)
-        c1 <- "one"
-    }()
-    go func() {
-        time.Sleep(2 * time.Second)
-        c2 <- "two"
-    }()
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+	go func() {
+		time.Sleep(1*time.Second + 500*time.Millisecond)
+		c3 <- "one half"
+	}()
 
-    for i := 0; i < 2; i++ {
-        select {
-        case msg1 := <-c1:
-            fmt.Println("received", msg1)
-        case msg2 := <-c2:
-            fmt.Println("received", msg2)
-        }
-    }
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		case msg3 := <-c3:
+			fmt.Println("received", msg3)
+		}
+	}
 }
